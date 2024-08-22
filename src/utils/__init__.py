23305aeb30b77a47a4342304dbda4fe423ab320a
Liu3420175 <1196201660@qq.com>
2024-08-22 18:57:34 +0800

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,12 +1,12 @@
 from .internet import search_on_baike
-from .loader import load_and_split_text
+from .loader import Loader
 from .binidx import MMapIndexedDataset, shell_command
 from .make_data import Jsonl2Binidx
 
 __all__ = [
-    'search_on_baike',
-    'load_and_split_text',
+    'Loader',
     'MMapIndexedDataset',
     'Jsonl2Binidx',
-    "shell_command"
+    'shell_command',
+    'search_on_baike',
 ]
